ab81dcdb1735e8c6493546e04c0fd57a8e086125
Nayara Vasques <129865342+NayaVasques@users.noreply.github.com>
2024-08-19 00:29:17 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,9 @@ from oauth2client.service_account import ServiceAccountCredentials
 # Google Sheets
 def connect_to_gsheets():
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-    creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
+    creds = ServiceAccountCredentials.from_json_keyfile_name("khronos.json", scope)
     client = gspread.authorize(creds)
-    sheet = client.open("Disponibilidades Cronograma").sheet1  # Abre a planilha
+    sheet = client.open("khronos").sheet1  # open file
     return sheet
 
 # add line
